2604a6edf5eb57bfc05d0e6e3cfc8ec115a6d8b7
KUMARASAMY Thagshayini <thagshayini.kumarasamy@consulting-for.accor.com>
2023-04-12 12:06:20 +0200

    improve lambda code

--- modules/api/lambda_code/package/index.py
+++ modules/api/lambda_code/package/index.py
@@ -1,6 +1,4 @@
 import os
-import boto3
-from botocore.exceptions import ClientError
 import getCredentials_layer as gc
 import psycopg2
 
@@ -10,33 +8,39 @@ REGION_NAME = os.environ['region_name']
 DB = os.environ['db']
 RAW_PATH = os.environ['raw_path']
 
-#Connect to the database and query it    
+#Connect to the database
+credential = gc.getCredentials(SECRET_NAME, REGION_NAME, DB)
+      
+connection = psycopg2.connect(user=credential['username'], password=credential['password'], host=credential['host'], database=credential['db'])
+cursor = connection.cursor()
+print ("Successfully connected to DB")
+
+#Query the database
 def lambda_handler(event, context):
     if event['rawPath'] == RAW_PATH:
       #Get the coordinates from the API query
       longitude = event['queryStringParameters']['longitude']
       latitude = event['queryStringParameters']['latitude']
-      
-      credential = gc.getCredentials(SECRET_NAME, REGION_NAME, DB)
-      
-      connection = psycopg2.connect(user=credential['username'], password=credential['password'], host=credential['host'], database=credential['db'])
-      cursor = connection.cursor()
-      print ("Successfully connected to DB")
-      
+
       query = """
       SELECT zone, niveau, mesurespart
       FROM localites
       INNER JOIN arrete ON localites.id = arrete.zone
       WHERE ST_Intersects('POINT({} {})'::geography::geometry, wkb_geometry);
       """.format(longitude,latitude)
-      cursor.execute(query)
-      results = cursor.fetchone()
-      #print (results[0])
+      try:
+        cursor.execute(query)
+        results = cursor.fetchone()
+        #print (results[0])
+        
+        cursor.close()
+        connection.commit()
+
+        print ("Sending results")
+        return results
       
-      cursor.close()
-      connection.commit()
-      
-      return results
+      except Exception as exeption:
+        print("Error: ", exeption)
       
     else:
-      return {"message" : "error"}
+      return {"Error" : "Wrong API path"}
