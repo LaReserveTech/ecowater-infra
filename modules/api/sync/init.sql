891975f3c4aa1584e84d7227d6c12516d9e56833
Florian Touya (Deuzu) <florian.touya@pm.me>
2023-05-05 23:11:18 +0200

    feat(sync): Add geozones sync

--- modules/api/sync/init.sql
+++ modules/api/sync/init.sql
@@ -1,28 +1,18 @@
 DROP TABLE IF EXISTS alert_subscription;
 DROP TABLE IF EXISTS restriction;
 DROP TABLE IF EXISTS decree;
-DROP TABLE IF EXISTS geozone;
 DROP TYPE IF EXISTS alert_level;
 DROP TYPE IF EXISTS restriction_level;
-DROP TYPE IF EXISTS water_type;
 
 ------
 
 CREATE TYPE alert_level AS ENUM ('alerte', 'alerte_renforcee', 'vigilance', 'crise');
 CREATE TYPE restriction_level AS ENUM ('interdiction', 'interdiction_exception', 'interdiction_plage_horaire', 'reduction', 'sensibilisation');
-CREATE TYPE water_type AS ENUM ('superficiel', 'souterraine');
-
-CREATE TABLE geozone (
-    id SERIAL PRIMARY KEY,
-    external_id VARCHAR(25) UNIQUE NOT NULL,
-    geozone GEOMETRY(POLYGON, 4326) NOT NULL,
-    water_type water_type NOT NULL
-);
 
 CREATE TABLE decree(
     id SERIAL PRIMARY KEY,
     external_id VARCHAR(25) NOT NULL UNIQUE,
-    geozone_external_id VARCHAR(25) NOT NULL,
+    geozone_id INT REFERENCES geozone (gid) NOT NULL,
     alert_level alert_level NOT NULL,
     start_date date NOT NULL,
     end_date date NOT NULL
@@ -47,5 +37,5 @@ CREATE TABLE restriction (
 CREATE TABLE alert_subscription (
     id SERIAL PRIMARY KEY,
     email VARCHAR(254) UNIQUE NOT NULL,
-    geozone_id INT REFERENCES geozone (id) NOT NULL
+    geozone_id INT REFERENCES geozone (gid) NOT NULL
 );
