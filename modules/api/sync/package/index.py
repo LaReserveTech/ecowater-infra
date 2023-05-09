0e948ea98f271c36dea23392b02a8adf129affad
maximonstre <39076714+maximonstre@users.noreply.github.com>
2023-05-09 21:55:17 +0200

    getdata from gouv

--- modules/api/sync/package/index.py
+++ modules/api/sync/package/index.py
@@ -7,6 +7,43 @@ import decree_provider
 import decree_repository
 import restriction_provider
 import restriction_repository
+import requests
+import json
+
+DATASET='643d5f985c230e2b786c5602'
+
+def getdata():
+
+# URL de l'API à appeler
+    url = 'https://demo.data.gouv.fr/api/1/datasets/' + DATASET
+
+    # Faire une requête GET pour récupérer le fichier
+    response = requests.get(url)
+
+    # Vérifier si la requête a réussi
+    if response.status_code == 200:
+        #print (response.content)
+        data = json.loads(response.text)
+        print( len(data['resources']))
+        nbfile = len(data['resources'])
+        for x in range(nbfile):
+            print( data['resources'][x]['url'])
+            file=data['resources'][x]['url']
+            response = requests.get(file)
+            if response.status_code == 200:
+        # Enregistrer le fichier sur le disque dur
+                with open(data['resources'][x]['title'], 'wb') as f:
+                    f.write(response.content)
+                print('Le fichier a été téléchargé avec succès.')
+            else:
+        # Afficher un message d'erreur si la requête a échoué
+                print('La requête a échoué avec le code d\'erreur', response.status_code)
+    else:
+        # Afficher un message d'erreur si la requête a échoué
+        print('La requête a échoué avec le code d\'erreur', response.status_code)
+    return None
+
+
 
 def lambda_handler(_event, _context):
     connection = psycopg2.connect(database="ecowater", user="ecowater", password="ecowater", host="pg", port="5432") # TODO use environment variables
@@ -15,6 +52,12 @@ def lambda_handler(_event, _context):
 
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
     logging.info('Connected to the database')
+
+    # Retrieve all files
+    getdata()
+
+    # todo replace the existing file reader by the file getted by the previous function
+
 
     # Decrees
     decrees_content = decree_provider.get()
