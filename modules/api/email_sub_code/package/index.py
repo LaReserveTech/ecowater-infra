300b1ff368f50dd7ea76c59428b16061db61586b
KUMARASAMY Thagshayini <thagshayini.kumarasamy@consulting-for.accor.com>
2023-05-30 00:00:35 +0200

    check content-type and parameters

--- modules/api/email_sub_code/package/index.py
+++ modules/api/email_sub_code/package/index.py
@@ -39,38 +39,70 @@ logging.debug("Connected to the database")
 
 #Insert email into the database
 def lambda_handler(event, context):
-  if event['rawPath'] == RAW_PATH:
-    #Get the coordinates from the API query
-    longitude = event['queryStringParameters']['longitude']
-    latitude = event['queryStringParameters']['latitude']
-    email = event['queryStringParameters']['email']
-
-    if validate_emails(email):
-      query = 'INSERT into alert_subscription (email, longitude, latitude, subscribed_at) VALUES (%s, %s, %s,  %s);'
-      parameters = (email, longitude, latitude, subscribtion_date)
+    if event['rawPath'] == RAW_PATH:
       
-      try:
-        cursor = connection.cursor()
-        cursor.execute(query, parameters)
-
-        cursor.close()
-        connection.commit()
-
-        logging.debug("Email successfully registered in the DB")
-
+      #Validate content-type
+      headers = event['headers']
+      
+      if headers.get('content-type') is None or headers.get('content-type') != "application/json":
+        logging.error("Content-type wasn't in the headers, or was not of type 'application/json'")
         return {
-          "statusCode": "202",
-          "body": "Email successfully registered in the DB"
+            "statusCode": "400",
+            "body": "Content-type should be 'application/json'"
         }
-
-      except Exception as exeption:
-        logging.error("Email not registered in the DB. Exception: %s", str(exeption))
+      
+      #Validate parameters
+      parameters = event['queryStringParameters']
+      if parameters.get('longitude') is not None and parameters.get('latitude') is not None and parameters.get('email') is not None:
+        logging.debug("The parameters are valid") #placeholder until long/lat format is checked
+        #if : #TODO Check long/lat format
+        #  logging.debug("The parameters are valid")
+        #else:
+        #  logging.error("Long and/or lat is not in decimal degrees (DD)")
+        #  return {
+        #      "statusCode": "422",
+        #      "body": "Longitude and/or latitude is not of the correct format. Must be decimal degrees (DD)"
+        #  }
+          
+      else:
+        logging.error("A parameter is missing")
+        return {
+            "statusCode": "400",
+            "body": "A parameter is missing. Parameters should be 'longitude','latitude' and 'email'"
+        }
+      
+      #Get the coordinates and email from the API query
+      longitude = parameters['longitude']
+      latitude = parameters['latitude']
+      email = parameters['email']
+      
+      #Validate email
+      if validate_emails(email):
+        query = 'INSERT into alert_subscription (email, longitude, latitude, subscribed_at) VALUES (%s, %s, %s,  %s);'
+        parameters = (email, longitude, latitude, subscribtion_date)
         
-    else:
-      return {
-          "statusCode": "422",
-          "body": "Unprocessable Entity : email is not valid"
-          } 
+        try:
+          cursor = connection.cursor()
+          cursor.execute(query, parameters)
+  
+          cursor.close()
+          connection.commit()
+  
+          logging.debug("Email successfully registered in the DB")
+  
+          return {
+            "statusCode": "202",
+            "body": "Email successfully registered in the DB"
+          }
+  
+        except Exception as exeption:
+          logging.error("Email not registered in the DB. Exception: %s", str(exeption))
+          
+      else:
+        return {
+            "statusCode": "422",
+            "body": "Unprocessable Entity : email is not valid"
+        } 
 
-  else:
-    return {"Error" : "Wrong API path"}
+    else:
+      return {"Error" : "Wrong API path"}
