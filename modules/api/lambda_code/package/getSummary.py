edabbe7f20d634fb8a0341f44f0e685f2e86b28e
Clara Ni <clara.ni@student-cs.fr>
2023-06-06 13:13:44 +0200

    Api: adapt result dict
    - results dict has a "niveau-alerte", a "pdf" and a "restrictions" fields

--- modules/api/lambda_code/package/getSummary.py
+++ modules/api/lambda_code/package/getSummary.py
@@ -1,7 +1,7 @@
 from data_provider import get_mocked_restrictions
 
 def create_summary(restrictions_data):
-  results_dict = {
+  restrictions_dict = {
     "sensibilisation": {},
     "reduction-prelevement": {},
     "interdiction-plage-horaire": {},
@@ -9,54 +9,41 @@ def create_summary(restrictions_data):
     "interdiction": {},
   }
 
-  # useful to understand how the data is structured
-  dict_by_level_restriction = {
-    "Sensibilisation": [],
-    "Réduction de prélèvement": [],
-    "Interdiction sur plage horaire": [],
-    "Interdiction sauf exception": [],
-    "Interdiction": []
-  }
-  
   for item in restrictions_data:
-    results_dict["niveau-alerte"] = item[0] # need to check if this is the expected behaviour
-
     formatted_restriction = {
-        "thematique": item[2],
-        "libelle-personnalise": item[6],
-        "en-savoir-plus": item[5],
+      "thematique": item[2],
+      "libelle-personnalise": item[6],
+      "en-savoir-plus": item[5],
     }
 
     if item[1] == "Sensibilisation":
-        dict_by_level_restriction["Sensibilisation"].append(item)
-        results_dict["sensibilisation"][item[3]] = formatted_restriction
+      restrictions_dict["sensibilisation"][item[3]] = formatted_restriction
 
     elif item[1] == "Réduction de prélèvement":
-        dict_by_level_restriction["Réduction de prélèvement"].append(item)
-        results_dict["reduction-prelevement"][item[3]] = formatted_restriction
-    
+      restrictions_dict["reduction-prelevement"][item[3]] = formatted_restriction
+
     elif item[1] == "Interdiction sur plage horaire":
-        dict_by_level_restriction["Interdiction sur plage horaire"].append(item)
-        results_dict["interdiction-plage-horaire"][item[3]] = {
-            "thematique": item[2],
-            "heure-debut": item[6],
-            "heure-fin": item[7],
-            "en-savoir-plus": item[5],
-        }
+      restrictions_dict["interdiction-plage-horaire"][item[3]] = {
+        "thematique": item[2],
+        "heure-debut": item[6],
+        "heure-fin": item[7],
+        "en-savoir-plus": item[5],
+      }
 
     elif item[1] == "Interdiction sauf exception":
-        dict_by_level_restriction["Interdiction sauf exception"].append(item)
-        results_dict["interdiction-sauf-exception"][item[3]] = formatted_restriction
+      restrictions_dict["interdiction-sauf-exception"][item[3]] = formatted_restriction
 
     elif item[1] == "Interdiction":
-        dict_by_level_restriction["Interdiction"].append(item)
-        results_dict["interdiction"][item[3]] = formatted_restriction
+      restrictions_dict["interdiction"][item[3]] = formatted_restriction
 
-  print(results_dict)
+  result_dict = {
+    "niveau-alerte": restrictions_data[0][0],
+    "pdf": "dummy-pdf.fr",
+    "restrictions": restrictions_dict
+  }
 
-  return results_dict
+  return result_dict
 
 
-    
 if __name__ == '__main__':
   create_summary(get_mocked_restrictions())
