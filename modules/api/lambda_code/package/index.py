e1bae57f839b5f268d46a00c6efe4689a3ad2496
clarani <clara.ni@student-cs.fr>
2023-06-07 09:57:22 +0200

    Api: adapt result dict (#13)
    - results dict has a "niveau-alerte", a "pdf" and a "restrictions" fields
    - add left join in the sql request + constraint on start_date and end_date

--- modules/api/lambda_code/package/index.py
+++ modules/api/lambda_code/package/index.py
@@ -15,7 +15,7 @@ RAW_PATH = os.environ['raw_path']
 
 #Connect to the database
 credential = gc.getCredentials(SECRET_NAME, REGION_NAME, DB)
-      
+
 connection = psycopg2.connect(user=credential['username'], password=credential['password'], host=credential['host'], database=credential['db'])
 connection.autocommit = True
 logging.debug("Connected to the database")
@@ -23,16 +23,16 @@ logging.debug("Connected to the database")
 #Query the database
 def lambda_handler(event, context):
     if event['rawPath'] == RAW_PATH:
-      #Get the coordinates from the API query
+      # get the coordinates from the API query
       longitude = event['queryStringParameters']['longitude']
       latitude = event['queryStringParameters']['latitude']
       situation = event['queryStringParameters']['situation']
 
       query = """
-      SELECT DISTINCT de.alert_level, re.restriction_level, re.theme, re.label, re.description, re.specification, re.from_hour, re.to_hour                                        
+      SELECT DISTINCT de.alert_level, re.restriction_level, re.theme, re.label, re.description, re.specification, re.from_hour, re.to_hour
       FROM geozone AS gz
       INNER JOIN decree AS de ON de.geozone_id = gz.id
-      INNER JOIN restriction AS re ON re.decree_id = de.id
+      LEFT JOIN restriction AS re ON re.decree_id = de.id
       WHERE ST_Contains(
           gz.geom,
           ST_SetSRID(
@@ -40,29 +40,28 @@ def lambda_handler(event, context):
                 4326
           )
       )
-      AND re.%s IS TRUE;
+      AND re.%s IS TRUE
+      AND de.start_date <= NOW()
+      AND NOW() <= de.end_date;
       """
       params = (longitude, latitude, AsIs(situation))
-       
+
       try:
         cursor = connection.cursor()
         cursor.execute(query, params)
         data = cursor.fetchall()
         print (connection.encoding)
-        
+
         cursor.close()
         connection.commit()
-        
-        # for restriction in restriction_level : 
-          #base64.b64encode(restriction).decode('utf-8')
-        
+
         # create the summary from the data
         results_dict = create_summary(data)
 
         return results_dict
-        
+
       except Exception as e:
         logging.error('Failed to fetch restrictions. User: %s, Longitude: %s, Latitude: %s, Exception: %s', situation, longitude, latitude, str(e))
-      
+
     else:
       return {"Error" : "Wrong API path"}
