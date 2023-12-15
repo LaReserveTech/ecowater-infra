248e64162435e51caad4c5fbff57d5bc3dbd7fb5
Clara Ni <clara.ni@student-cs.fr>
2023-12-15 23:14:36 +0100

    feat(sync): Create relevant emails

---
+++ src/types.ts
@@ -0,0 +1,2 @@
+export type AlertLevel = 'vigilance' | 'alerte' | 'alerte renforcée' | 'crise'
+export type MailType = 'decree_repeal' | 'decree_creation' | 'decree_update'
--- src/utils/create-email.ts
+++ src/utils/create-email.ts
@@ -1,37 +1,49 @@
+import { AlertLevel, MailType } from '../types'
+
+type MailUser = {
+  Email: string
+  Name: string
+}
 export type Mail = {
-  From: {
-    Email: string
-    Name: string
-  }
-  To: [
-    {
-      Email: string
-      Name: string
-    }
-  ]
+  From: MailUser
+  To: MailUser[]
   TemplateID: number
   TemplateLanguage: boolean
   Subject: string
   Variables: {
-    niveau: string
+    mailType: MailType
+    niveau?: string
   }
 }
 
-export const createEmail = (emailAddress: string): Mail => ({
-  From: {
-    Email: 'florian@alerte-secheresse.fr',
-    Name: 'Alerte Sécheresse',
-  },
-  To: [
-    {
-      Email: emailAddress,
-      Name: emailAddress,
-    },
-  ],
-  TemplateID: 4732985,
-  TemplateLanguage: true,
-  Subject: 'renforcée',
-  Variables: {
-    niveau: 'maximale',
-  },
+const ALERT_LEVEL_TRAD = Object.freeze({
+  vigilance: 'de vigilance',
+  alerte: "d'alerte",
+  'alerte renforcée': "d'alerte renforcée",
+  crise: 'de crise',
 })
+
+export const createEmail = (emailAddress: string, mailType: MailType, alertLevel: AlertLevel | undefined): Mail => {
+  return {
+    From: {
+      Email: 'florian@alerte-secheresse.fr',
+      Name: 'Alerte Sécheresse',
+    },
+    To: [
+      {
+        Email: emailAddress,
+        Name: emailAddress,
+      },
+    ],
+    TemplateID: 5454985,
+    TemplateLanguage: true,
+    Subject:
+      mailType === 'decree_repeal'
+        ? "Les restrictions de votre territoire viennent d'être levées"
+        : "Votre territoire passe en état {{var:niveau:''}}",
+    Variables: {
+      mailType,
+      niveau: alertLevel ? ALERT_LEVEL_TRAD[alertLevel] : undefined,
+    },
+  }
+}
